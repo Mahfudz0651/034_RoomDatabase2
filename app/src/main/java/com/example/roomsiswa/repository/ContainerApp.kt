07ff7ff7cd7ec2e4b2ae2c649600e40fb311bf60
TC1034 <mahfudzsiddiq0@gmail.com>
2023-12-06 17:24:09 +0700

    Add class OfflineRepositoriSiswa

--- app/src/main/java/com/example/roomsiswa/repository/ContainerApp.kt
+++ app/src/main/java/com/example/roomsiswa/repository/ContainerApp.kt
@@ -1,7 +1,7 @@
 package com.example.roomsiswa.repository
 
 import android.content.Context
-import com.example.roomsiswa_aplikasisiswa.data.DatabaseSiswa
+import com.example.roomsiswa.data.DatabaseSiswa
 
 interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
--- app/src/main/java/com/example/roomsiswa/repository/OfflineRepositoriSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/repository/OfflineRepositoriSiswa.kt
@@ -7,4 +7,12 @@ import kotlinx.coroutines.flow.Flow
 class OfflineRepositoriSiswa(private val siswaDao: SiswaDao):RepositoriSiswa {
     override fun getAllsiswaStream(): Flow<List<Siswa>> = siswaDao.getALLSiswa()
 
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
+
 }
