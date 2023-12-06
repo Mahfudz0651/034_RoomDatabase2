f4b1e0bb3862988d142aa6f0a32438b2fc657e22
TC1034 <mahfudzsiddiq0@gmail.com>
2023-12-06 19:16:26 +0700

    Add fun EntrySiswaBody in HalamanEntry.kt

--- app/src/main/java/com/example/roomsiswa/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/Halaman/HalamanEntry.kt
@@ -1,2 +1,22 @@
 package com.example.roomsiswa.Halaman
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.roomsiswa.R
+import com.example.roomsiswa.model.DetailSiswa
+import com.example.roomsiswa.model.UIStateSiswa
+import com.example.roomsiswa.navigasi.DestinasiNavigai
+
+object DestinasiEntry: DestinasiNavigai {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
+@Composable
+fun EntrySiswaBody(
+    uiStateSiswa: UIStateSiswa,
+    onSiswaValueChange: (DetailSiswa) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}
