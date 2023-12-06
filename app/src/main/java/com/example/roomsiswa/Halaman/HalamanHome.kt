1e447d942f80006eb6e7f614f58e41ef39dc9f16
TC1034 <mahfudzsiddiq0@gmail.com>
2023-12-06 21:32:03 +0700

    Add fun ListSiswa in HalamanHome..kt

--- app/src/main/java/com/example/roomsiswa/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/Halaman/HalamanHome.kt
@@ -3,6 +3,8 @@ package com.example.roomsiswa.Halaman
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -102,4 +104,20 @@ fun BodyHome(
 fun ListSiswa(
     itemSiswa: List<Siswa>,
     modifier: Modifier= Modifier
+){
+    LazyColumn(modifier = Modifier) {
+        items(items = itemSiswa, key = {it.id}){
+            person ->
+            DataSiswa(
+                siswa = person,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
+}
+@Composable
+fun DataSiswa(
+    siswa: Siswa,
+    modifier: Modifier = Modifier
 ){}
